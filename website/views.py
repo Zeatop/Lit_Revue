d711ad911498a056a6eb04b96d00d2faec2e6e6b
léo jackson <leo_jackson@hotmail.fr>
2025-01-08 14:59:36 +0100

    Restauration version antécédantes

--- website/views.py
+++ website/views.py
@@ -13,15 +13,10 @@ from operator import attrgetter
 from django import forms
 from django.contrib import messages
 from django.db.models import Q
-
-
-
 User = get_user_model()
-
 class UserProfileView(ListView):
     template_name = 'website/user_profile.html'
     context_object_name = 'contributions'
-
     def get_queryset(self):
         user = get_object_or_404(User, username=self.kwargs['username'])
         tickets = Ticket.objects.filter(user=user)
@@ -32,35 +27,29 @@ class UserProfileView(ListView):
             reverse=True
         )
         return contributions
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         profile_user = get_object_or_404(User, username=self.kwargs['username'])
         context['profile_user'] = profile_user
         context['is_following'] = self.request.user.is_authenticated and self.request.user.followed_users.filter(pk=profile_user.pk).exists()
         return context
-
 class SubscribeView(LoginRequiredMixin, View):
     def post(self, request, username):
         user_to_follow = get_object_or_404(User, username=username)
         request.user.subscribe(user_to_follow)
         messages.success(request, f'Vous suivez maintenant {username}.')
         return redirect('website:user_profile', username=username)
-
 class UnsubscribeView(LoginRequiredMixin, View):
     def post(self, request, username):
         user_to_unfollow = get_object_or_404(User, username=username)
         request.user.unsubscribe(user_to_unfollow)
         messages.success(request, f'Vous ne suivez plus {username}.')
         return redirect('website:user_profile', username=username)
-
 class FollowedUsersView(LoginRequiredMixin, ListView):
     template_name = 'website/followed_users.html'
     context_object_name = 'followed_users'
-
     def get_queryset(self):
         return self.request.user.followed_users.all()
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         search_query = self.request.GET.get('search', '')
@@ -80,7 +69,6 @@ class HomeView(TemplateView):
     Hérite de TemplateView pour une simple affichage de template.
     """
     template_name = 'website/home.html'
-
     def get_context_data(self, **kwargs):
         """
         Ajoute des formulaires d'inscription et de connexion au contexte.
@@ -89,69 +77,64 @@ class HomeView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['register_form'] = RegisterForm()
         context['login_form'] = AuthenticationForm()
-
-        tickets = Ticket.objects.all()
-        reviews = Review.objects.all()
-        combined_list = sorted(
-            chain(tickets, reviews),
-            key=attrgetter('time_created'),
-            reverse=True
-        )
-        
-        context['combined_feed'] = combined_list[:10]
-        context['user'] = self.request.user
+        user = self.request.user
+        if user.is_authenticated:
+            reviews = Review.objects.filter(
+                Q(user__in=user.followed_users.all()) |
+                Q(user=user) | 
+                Q(ticket__user=user)
+                )
+            tickets = Ticket.objects.filter(
+                Q(user__in=user.followed_users.all()) | Q(user=user)
+                )        
+            combined_list = sorted(
+                chain(tickets, reviews),
+                key=attrgetter('time_created'),
+                reverse=True
+            )
+            
+            context['combined_feed'] = combined_list[:10]
+            context['user'] = user
         return context
-
 class ActionContextMixin:
     """
     Mixin pour récupérer l'action en cours d'execution par l'utilisateur 
     """
-
     action = None
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['view'] = {'action': self.action}
         return context
-
 class TicketCreateView(LoginRequiredMixin, ActionContextMixin, CreateView):
     model = Ticket
     form_class = NewTicketForm
     template_name = 'website/Ticket.html'
     success_url = reverse_lazy('website:ticket_list')
     action='create'
-
-
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
-
 class TicketUpdateView(LoginRequiredMixin, UserPassesTestMixin, ActionContextMixin, UpdateView):
     model = Ticket
     form_class = NewTicketForm
     template_name = 'website/Ticket.html'
     success_url = reverse_lazy('website:ticket_list')
     action='edit'
-
     def test_func(self):
         ticket = self.get_object()
         return self.request.user == ticket.user
-
 class TicketDeleteView(LoginRequiredMixin, UserPassesTestMixin, ActionContextMixin, DeleteView):
     model = Ticket
     template_name = 'website/Ticket.html'
     success_url = reverse_lazy('website:ticket_list')
     action='delete'
-
     def test_func(self):
         ticket = self.get_object()
         return self.request.user == ticket.user
-
 class TicketDetailView(LoginRequiredMixin, DetailView):
     model = Ticket
     action='detail'
     template_name = 'website/Ticket.html'
-
 class TicketListView(LoginRequiredMixin, ListView):
     model = Ticket
     template_name = 'website/TicketList.html'
@@ -159,25 +142,21 @@ class TicketListView(LoginRequiredMixin, ListView):
     
     def get_queryset(self):
         return Ticket.objects.all()
-
 class ReviewCreateView(LoginRequiredMixin, ActionContextMixin, CreateView):
     action = 'create'
     model = Review
     template_name = 'website/Review.html'
     success_url = reverse_lazy('website:review_list')
-
     def get_form_class(self):
         if self.request.GET.get('ticket'):
             return ReviewForm
         return CombinedTicketReviewForm
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         ticket_id = self.request.GET.get('ticket')
         if ticket_id:
             context['ticket'] = get_object_or_404(Ticket, pk=ticket_id)
         return context
-
     def form_valid(self, form):
         if isinstance(form, ReviewForm):
             form.instance.user = self.request.user
@@ -200,20 +179,17 @@ class ReviewCreateView(LoginRequiredMixin, ActionContextMixin, CreateView):
                 ticket=ticket
             )
             return redirect(self.success_url)
-
     def get_initial(self):
         initial = super().get_initial()
         ticket_id = self.request.GET.get('ticket')
         if ticket_id:
             initial['ticket'] = ticket_id
         return initial
-
     def get_form_kwargs(self):
         kwargs = super().get_form_kwargs()
         if not self.request.GET.get('ticket'):
             kwargs.pop('instance', None)
         return kwargs
-
     def get_form(self, form_class=None):
         form_class = self.get_form_class()
         kwargs = self.get_form_kwargs()
@@ -229,34 +205,33 @@ class ReviewCreateView(LoginRequiredMixin, ActionContextMixin, CreateView):
         else:
             form = form_class(**kwargs)
         
-        return form
-    
+        return form    
 class ReviewUpdateView(LoginRequiredMixin, UserPassesTestMixin, ActionContextMixin, UpdateView):
-    """
-    Vue pour mettre à jour une critique existante.
-    UserPassesTestMixin vérifie que l'utilisateur a le droit de modifier cette critique.
-    """
     model = Review
-    form_class = ReviewForm
     template_name = 'website/Review.html'
-    action='edit'
-
-
+    action = 'edit'
+    form_class = ReviewForm
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        review = self.get_object()
+        context['review'] = review
+        context['ticket'] = review.ticket  # Ajouter le ticket au contexte
+        return context
+    
     def test_func(self):
-        """
-        Vérifie que l'utilisateur actuel est l'auteur de la critique.
-        Cette méthode est appelée par UserPassesTestMixin.
-        """
         review = self.get_object()
         return self.request.user == review.user
-
-
+        
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+        # Cacher le champ ticket car on ne peut pas le modifier
+        if 'ticket' in form.fields:
+            form.fields['ticket'].widget = forms.HiddenInput()
+        return form
+    
     def get_success_url(self):
-        """
-        Définit l'URL de redirection après la mise à jour réussie d'une critique.
-        """
         return reverse_lazy('website:review_detail', kwargs={'pk': self.object.pk})
-
 class ReviewDeleteView(LoginRequiredMixin, UserPassesTestMixin, ActionContextMixin, DeleteView):
     """
     Vue pour supprimer une critique.
@@ -264,16 +239,14 @@ class ReviewDeleteView(LoginRequiredMixin, UserPassesTestMixin, ActionContextMix
     """
     model = Review
     template_name = 'website/Review.html'
-    success_url = reverse_lazy('user_reviews')  # Assurez-vous que cette URL existe
+    success_url = success_url = reverse_lazy('website:review_list')
     action='delete'
-
     def test_func(self):
         """
         Vérifie que l'utilisateur actuel est l'auteur de la critique.
         """
         review = self.get_object()
         return self.request.user == review.user
-
 class ReviewDetailView(LoginRequiredMixin, DetailView):
     """
     Vue pour afficher les détails d'une critique.
@@ -282,7 +255,6 @@ class ReviewDetailView(LoginRequiredMixin, DetailView):
     model = Review
     action='detail'
     template_name = 'website/Review.html'
-
 class ReviewListView(LoginRequiredMixin, ListView):
     model = Review
     template_name = 'website/ReviewList.html'
@@ -290,4 +262,3 @@ class ReviewListView(LoginRequiredMixin, ListView):
     
     def get_queryset(self):
         return Review.objects.all()
-    
